2739cacc23e054466a7bbdb990312eddbec3b5e1
Muneshkumar12 <munesh788mk@gmail.com>
2023-09-04 15:26:03 +0500

    this is 2nd DOM assignment

---
+++ pages/index.js
@@ -0,0 +1,65 @@
+// var main = document.getElementById("main");
+// var row1 = document.getElementById("row-1");
+// var row2 = document.getElementById("row-2");
+// var nav = document.createElement("div");
+// var admin = document.createElement("h3")
+// var button = document.createElement("button");
+// var empty = document.createElement("div")
+// var rowData = document.createElement("div");
+// var tood = document.createElement("div");
+// var search = document.createElement("div");
+// var searchInp = document.createElement("input");
+// var submitBut = document.createElement("button");
+// var sectionThree = document.createElement("div");
+// var image1 = "file:///F:/Web%20development/Assignment/JavaScript/assignment-15/pages/images/edit.png";
+// var image2 = "file:///F:/Web%20development/Assignment/JavaScript/assignment-15/pages/images/delete.png";
+// // section 3 data 1
+// var dataOne = document.createElement("div")
+// var dogSection = document.createElement("div");
+// var dogInp = document.createElement("input");
+// var dogSpan = document.createElement("span");
+// var imgSec = document.createElement("div");
+// var imgOne = document.createElement("img");
+// var imgTwo = document.createElement("img");
+
+// row1.appendChild(nav);
+// nav.appendChild(admin);
+// nav.appendChild(button);
+// row2.appendChild(empty);
+// row2.appendChild(rowData);
+// rowData.appendChild(tood);
+// rowData.appendChild(search);
+// rowData.appendChild(sectionThree);
+// // section 3 data 1
+// sectionThree.appendChild(dataOne);
+// dataOne.appendChild(dogSection);
+// dataOne.appendChild(imgSec);
+// dogSection.appendChild(dogInp);
+// dogSection.appendChild(dogSpan);
+// search.appendChild(searchInp);
+// search.appendChild(submitBut);
+// imgSec.appendChild(imgOne);
+// imgSec.appendChild(imgTwo);
+
+// nav.setAttribute("id" , "header");
+// empty.setAttribute("class" , "col");
+// rowData.setAttribute("class" , "col-5 section-2-data");
+// tood.setAttribute("class" , "todos");
+// search.setAttribute("class" , "search");
+// searchInp.setAttribute("type" , "search");
+// sectionThree.setAttribute("class" , "section-3");
+// // section 3 data 1 
+// dataOne.setAttribute("class" , "section-3-data");
+// dogInp.setAttribute("type" , "checkbox")
+// imgOne.setAttribute("src" , image1)
+// imgOne.setAttribute("alt" , "img")
+// imgOne.setAttribute("class" , "img-1")
+// imgTwo.setAttribute("src" , image2)
+// imgTwo.setAttribute("alt" , "img")
+// imgTwo.setAttribute("class" , "img-2")
+
+// admin.innerHTML = ("admin");
+// button.innerHTML = ("logout");
+// tood.innerHTML = ("Todod(5)");
+// submitBut.innerHTML = ("Submit");
+// dogSpan.innerHTML = ("Walk the dog");
